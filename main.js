37062d52d52d4beed19f3547773361a8d986d3d8
Wesley Santos <wesleyssantos27@gmail.com>
2020-07-14 02:37:33 -0300

    Video concluído
    Realizada toda a instrução do vídeo. Agora resta entender e estilizar.

--- main.js
+++ main.js
@@ -1,17 +1,78 @@
 const cardBoard = document.querySelector("#cardboard");
 const images = [
-    'planeta.jpg'
-];
+    'angular.svg',
+    'aurelia.svg',
+    'backbone.svg',
+    'ember.svg',
+    'react.svg',
+    'vue.svg'
+]
 
 let cardHTML = '';
 
 images.forEach(img => {
     cardHTML += `
-        <div>
-            <img src="img/${img}">
-            <img src="img/capa1.svg">
-        <div>
-    `
+        <div class="memory-card" data-card="${img}">
+            <img class="front-face" src="dark img/${img}">
+            <img class="back-face" src="dark img/js-badge.svg">
+        </div>
+    `;
 });
 
-cardBoard.innerHTML = cardHTML;
+cardBoard.innerHTML = cardHTML + cardHTML;
+
+/** Final do HTML */
+
+const cards = document.querySelectorAll('.memory-card');
+let firstCard, secondCard;
+let lockCards = false;
+
+
+function flipCard(){
+    if(lockCards) return false;
+    this.classList.add('flip')
+
+    if(!firstCard){
+       firstCard = this;
+
+       return false;
+    }
+
+    secondCard = this;
+    checkForMatch (this);
+}
+
+function checkForMatch(){
+    let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+    !isMatch ? disableCards() : resetCards(isMatch);
+}
+
+function disableCards(){
+    lockCards = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetCards();
+    },  1000);
+}
+
+(function shuffle(){
+    cards.forEach(card => {
+        let rand = Math.floor(Math.random() * 12);
+        card.style.order = rand;
+    })
+})()
+
+function resetCards(isMatch = false){
+    if(isMatch){
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+    }
+        
+    [firstCard, secondCard, lockCards] = [null, null, false];
+}
+
+cards.forEach(card => card.addEventListener('click',flipCard))
